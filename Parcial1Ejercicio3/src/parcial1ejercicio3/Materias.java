a6c7b0b81cf1f85fc8e9ae1632e51ea1570d5765
joseeduardob <76182948+joseeduardob@users.noreply.github.com>
2021-09-25 12:00:50 -0600

    Ejercicio 3 FINAL

---
+++ Parcial1Ejercicio3/src/parcial1ejercicio3/Materias.java
@@ -0,0 +1,21 @@
+package parcial1ejercicio3;
+
+public class Materias extends Estudiante{
+    private int cdMateria, Grado;
+    private String NMateria;
+    public double NotaFinal;   
+    public Materias() {}
+    public Materias(int co, String no, int CodigoMateria, String ap, String NMateria, int Gra) {     
+        this.cdMateria = CodigoMateria;
+        this.NMateria = NMateria;
+        this.Grado = Gra;
+    }
+    public double getNotaFinal() { return NotaFinal;}
+    public void setNotaFinal(double NotaFinal){this.NotaFinal = NotaFinal;}
+    public int getCodMateria(){return cdMateria;}
+    public String getMateria(){return NMateria;}
+    public int getGrado(){return Grado;}
+    public void setCodigoMateria(int CodigoMateria){this.cdMateria = CodigoMateria;}
+    public void setMateria(String Materia){this.NMateria = Materia;}
+    public void setGrado(int Grado){this.Grado = Grado;}
+}
